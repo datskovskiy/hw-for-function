b8c2a329401bf115e3632bd1b6dfdc9f939fb194
anton.datskovskiy <datskovskiy.anton@gmail.com>
2021-04-08 09:35:06 +0300

    Implement code

--- src/functions.js
+++ src/functions.js
@@ -1,29 +1,64 @@
 function isBigger(a, b) {
-  // Your implementation here
+  return a - b > 0;
 };
 
 function isSmaller(a, b) {
-  // Your implementation here
+  return !isBigger(a, b);
 };
 
 function getMin(...numbers) {
-  // Your implementation here
+  return numbers.reduce((min, current) => current < min ? current : min, numbers[0]);
 }
 
 function makeNumber(string) {
-  // Your implementation here
+  return string.replace(/\D/g, '');
 };
 
 function countNumbers(string) {
-  // Your implementation here
+  let stringNumbers = makeNumber(string);
+
+  let obj = {};
+
+  for (let i = 0; i <= 9; i++) {
+    let regexp = new RegExp(`${i}`, 'g');
+    let regExpResult = stringNumbers.match(regexp);
+
+    if (regExpResult !== null)
+      obj[i] = regExpResult.length;
+  }
+
+  return obj;
 };
 
 function pipe(number, ...functions) {
-  // Your implementation her
+  for (let i = 0; i < functions.length; i++)
+    number = functions[i](number);
+
+  return number;
 }
 
 function isLeapYear(date) {
-  // Your implementation here
+  let dt;
+
+  if (typeof date === 'string') {
+    if (isNaN(Date.parse(date)))
+      return 'Invalid Date';
+      
+    dt = new Date(Date.parse(date));
+  }
+
+  if (typeof date === 'number') {
+    dt = new Date(date);
+
+    if (isNaN(dt))
+      return 'Invalid Date';
+  }
+
+  let year = dt.getFullYear();
+
+  return ((year % 4 === 0) && (year % 100 !== 0)) || (year % 400 === 0)
+    ? `${year} is a leap year`
+    : `${year} is not a leap year`;
 };
 
 
